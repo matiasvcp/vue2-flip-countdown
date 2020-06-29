640b02d7afc9e3bde88043fd69df504766f51f1f
matiasvcp <47899293+matiasvcp@users.noreply.github.com>
2020-06-29 10:55:42 -0300

    Update FlipCountdown.vue
    dfg

--- src/FlipCountdown.vue
+++ src/FlipCountdown.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container flip-clock">
     <div class="d-row">
+      <div class="p-2 " >Reloj nuevo</div>
       <div class="p-2 " v-for="data in timeData">{{data.current | twoDigits}}</div>
     </div>
     <template
